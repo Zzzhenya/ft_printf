52a419893719aa4028d6976f6a6e4aa86ec4c655
Shenya De Silva <sde-silv@c4a9c1.42berlin.de>
2023-06-28 15:24:15 +0200

    Updated with %s

--- main.c
+++ main.c
@@ -7,8 +7,8 @@ int ft_printf(const char *, ...);
 
 int main(void)
 {
-	printf("Ft: %d\n", ft_printf("Hello!"));
-	printf("Or: %d\n", printf("Hello!"));
+	printf("Ft: %d\n", ft_printf("Hello!%s","World!"));
+	printf("Or: %d\n", printf("Hello!%s","World!"));
 	return (0);
 }
 
@@ -16,14 +16,25 @@ int ft_printf(const char *str, ...)
 {
     va_list arg_ptr;
     int     i;
+    int     ret;
+    char    *ret_str;
 
     i = 0;
+    ret = 0;
     va_start(arg_ptr, str);
     while (i < strlen(str))
     {
-        write(1, &str[i], 1);
-        i ++;
+        if (str[i] == '%' )
+        {
+            i = i + 2; 
+            ret_str = va_arg(arg_ptr, char *);
+            ft_putstr_fd(ret_str, 1);
+            ret = ret + strlen(ret_str);
+        }
+        else
+            ret = ret + write(1, &str[i], 1);
+        i++;
     }
     va_end(arg_ptr);    
-    return (i);
+    return (ret);
 }
