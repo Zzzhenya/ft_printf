1ba2b103eadf08c1f82f1bc3e205ff2abdbe721a
Shenya De Silva <shenya44@outlook.com>
2023-07-09 22:18:10 +0200

    Consolidated i and d tests

--- main.c
+++ main.c
@@ -1,131 +1,163 @@
 #include <stdio.h>
 #include <limits.h>
+#include <string.h>
 #include "ft_printf.h"
 
-int main(void)
+static int tests_without_specifier(void)
 {
-	char *s;
-	s = 0;
-	printf("\n====== Test without specifier ======\n\n");
+	printf("\n====== Test without specifier ===========================\n\n");
 /*
 	//seg fault
 	printf("				Test00:	0:\n");
 	printf("  Ft: %d\n", ft_printf(0));
 	printf("  Or: %d\n", printf(0));
 */
-	printf("				Test01:	\"\":\n");
-	printf("  Ft: %d\n", ft_printf(""));
-	printf("  Or: %d\n", printf(""));
-	printf("				Test02:	Hello World!:\n");
-	printf("  Ft: %d\n", ft_printf("Hello World!"));
-	printf("  Or: %d\n", printf("Hello World!"));
-	printf("				Test03:	\"\\:\n");
-	printf("  Ft: %d\n", ft_printf("\"\\"));
-	printf("  Or: %d\n", printf("\"\\"));
+	printf("			Test01:	\"\":");
+	if (ft_printf("") == printf(""))
+		printf("				= OK =\n");
+	else
+		printf("					= OO =\n");
+	printf("			Test02:	Hello World!:\n");
+	if (ft_printf("Hello World!\n") == printf("Hello World!\n"))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	printf("			Test03:	\"\\:\n");
+	if (ft_printf("\"\\\n") == printf("\"\\\n"))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	return (0);
+}
 
-	printf("\n====== Tests for %%s ======\n\n");
+static int tests_for_strings(void)
+{
+	char *s;
+	s = 0;
 
-	printf("				Test01: 0:\n");
-	printf("  Or: %d\n",printf("%s", s));
-	printf("  Ft: %d\n",ft_printf("%s", s));
-	printf("				Test02: \"%%s\",\"\":\n");
-	printf("  Or: %d\n",printf("%s", ""));
-	printf("  Ft: %d\n",ft_printf("%s", ""));
-	printf("				Test03:	%%s	and %%:\n");
-	printf("  Ft: %d\n", ft_printf("Hello!%sA%%BB", "World!"));
-	printf("  Or: %d\n", printf("Hello!%sA%%BB", "World!"));
-	printf("				Test04: \"{%%s}\", \"\":\n");
-	printf("  Or: %d\n",printf("{%s}", ""));
-	printf("  Ft: %d\n",ft_printf("{%s}", ""));
-	
-// %c
-	printf("\n====== Tests for %%c ======\n\n");
+	printf("\n====== Tests for %%s ============================\n\n");
 
-	printf("				Test01: ascii range :\n");
-	printf("  Ft: %d\n", ft_printf("Hello%c", '!'));
-	printf("  Or: %d\n", printf("Hello%c", '!'));
-	printf("				Test02: out of ascii range:\n");
-	printf("  Ft: %d\n", ft_printf("Hello%c", -10));
-	printf("  Or: %d\n", printf("Hello%c", -10));
-	printf("				Test03: 0 :\n");
-	printf("  Ft: %d\n", ft_printf("Hello%c", 0));
-	printf("  Or: %d\n", printf("Hello%c", 0));;
-	printf("				Test04: string :\n");
-	printf("  Ft: %d\n", ft_printf("Hello%c", "252"));
+	printf("			Test01: 0:\n");
+	if (ft_printf("%s\n", s) == printf("%s\n", s))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	printf("			Test02: \"%%s\",\"\":\n");
+	if (ft_printf("%s\n", "") == printf("%s\n", ""))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	printf("			Test03:	%%s	and %%:\n");
+	if ( ft_printf("Hello!%sA%%BB\n", "World!") == printf("Hello!%sA%%BB\n", "World!"))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	printf("			Test04: \"{%%s}\", \"\":\n");
+	if ( ft_printf("{%s}\n", "") == printf("{%s}\n", ""))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	return(0);
+}
+
+static int tests_for_chars(void)
+{
+	// %c
+	printf("\n====== Tests for %%c =================================\n\n");
+
+	printf("			Test01: ascii range :\n");
+	if ( ft_printf("Hello%c\n", '!') == printf("Hello%c\n", '!'))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	printf("			Test02: out of ascii range:\n");
+	if ( ft_printf("Hello%c\n", -10) == printf("Hello%c\n", -10))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+
+	printf("			Test03: 0 :\n");
+	if (  ft_printf("Hello%c\n", 0) == printf("Hello%c\n", 0))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
+	printf("			Test04: string :\n");
+	printf("  Ft: %d\n", ft_printf("Hello%c\n", "252"));
+	printf("						= Different from printf =\n");
 //	printf("  Or: %d\n", printf("Hello%c", "252"));
+	return (0);
+}
 
-// %d
-	printf("\n====== Tests for %%d ======\n\n");
+static int tests_for_ints(char *str)
+{
 
-
-	printf("				Test00: -1:\n");
-	printf("  Ft: %d\n", ft_printf("%d", -1));
-	printf("  Or: %d\n", printf("%d", -1));
+	printf("\n==================%%%c%s",str[1],"==================\n\n");
 
 	printf("				Test01: 42:\n");
-	printf("  Ft: %d\n", ft_printf("%d", 42));
-	printf("  Or: %d\n", printf("%d", 42));
+	if ( ft_printf(str, 42) == printf(str, 42))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 	printf("				Test02: \"%%d\", 0:\n");
-	printf("  Ft: %d\n", ft_printf("%d", 0));
-	printf("  Or: %d\n", printf("%d", 0));
+	if (ft_printf(str, 0)  == printf(str, 0))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 	printf("				Test03: -42:\n");
-	printf("  Ft: %d\n", ft_printf("%d", -42));
-	printf("  Or: %d\n", printf("%d", -42));
+	if (ft_printf(str, -42) == printf(str, -42))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 	printf("				Test04: INT_MAX:\n");
-	printf("  Ft: %d\n", ft_printf("%d", INT_MAX));
-	printf("  Or: %d\n", printf("%d", INT_MAX));
+	if (ft_printf(str, INT_MAX) == printf(str, INT_MAX))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 	printf("				Test05: INT_MIN:\n");
-	printf("  Ft: %d\n", ft_printf("%d", INT_MIN));
-	printf("  Or: %d\n", printf("%d", INT_MIN));
+	if (ft_printf(str, INT_MIN) == printf(str, INT_MIN))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 	printf("				Test06: +42:\n");
-	printf("  Ft: %d\n", ft_printf("%d", +42));
-	printf("  Or: %d\n", printf("%d", +42));
+	if (ft_printf(str, +42) == printf(str, +42))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 	printf("				Test07: \"Hello%%d\", 0 :\n");
-	printf("  Ft: %d\n", ft_printf("Hello%d", 0));
-	printf("  Or: %d\n", printf("Hello%d", 0));
+	if (str[1] == 'd')
+		if (ft_printf("Hello%d\n", 0) == printf("Hello%d\n", 0))
+			printf("								= OK =\n");
+		else
+			printf("								= OO =\n");
+	else
+		if (ft_printf("Hello%i\n", 0) == printf("Hello%i\n", 0))
+			printf("								= OK =\n");
+		else
+			printf("								= OO =\n");
 	printf("				Test08: Hexa 0x000000a7:\n");
-	printf("  Ft: %d\n", ft_printf("%d", 0x000000a7));
-	printf("  Or: %d\n", printf("%d", 0x000000a7));
+	if (ft_printf(str, 0x000000a7) == printf(str, 0x000000a7))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 	printf("				Test09: Leading zeros 0000005 :\n");
-	printf("  Ft: %d\n", ft_printf("%d", 0000005));
-	printf("  Or: %d\n", printf("%d", 0000005));
+	if (ft_printf(str, 0000005) == printf(str, 0000005))
+		printf("								= OK =\n");
+	else
+		printf("								= OO =\n");
 /*	printf("				Test10: printf(0) :\n");
 	printf("  Ft: %d\n", ft_printf(ft_printf(0)));
 	printf("  Or: %d\n", printf(printf(0)));
-*/
+*/	
+	return(0);
+}
 
-	printf("\n====== Tests for %%i ======\n\n");
-
-	printf("				Test01: 42:\n");
-	printf("  Ft: %d\n", ft_printf("%i", 42));
-	printf("  Or: %d\n", printf("%i", 42));
-	printf("				Test02: 0:\n");
-	printf("  Ft: %d\n", ft_printf("%i", 0));
-	printf("  Or: %d\n", printf("%i", 0));
-	printf("				Test03: -42:\n");
-	printf("  Ft: %d\n", ft_printf("%i", -42));
-	printf("  Or: %d\n", printf("%i", -42));
-	printf("				Test04: INT_MAX:\n");
-	printf("  Ft: %d\n", ft_printf("%i", INT_MAX));
-	printf("  Or: %d\n", printf("%i", INT_MAX));
-	printf("				Test05: INT_MIN:\n");
-	printf("  Ft: %d\n", ft_printf("%i", INT_MIN));
-	printf("  Or: %d\n", printf("%i", INT_MIN));
-	printf("				Test06: +42:\n");
-	printf("  Ft: %d\n", ft_printf("%i", +42));
-	printf("  Or: %d\n", printf("%i", +42));
-	printf("				Test07: 0 :\n");
-	printf("  Ft: %d\n", ft_printf("Hello%i", 0));
-	printf("  Or: %d\n", printf("Hello%i", 0));
-	printf("				Test08: Hexa 0x000000a7:\n");
-	printf("  Ft: %d\n", ft_printf("%i", 0x000000a7));
-	printf("  Or: %d\n", printf("%i", 0x000000a7));
-	printf("				Test09: Leading zeros 0000005 :\n");
-	printf("  Ft: %d\n", ft_printf("%i", 0000005));
-	printf("  Or: %d\n", printf("%i", 0000005));
-//	int width = 5;
-//	int num = 10;
-//	printf("%*d\n", width, num);
-//	printf("%2$*1$d\n", width, num);
+int main(void)
+{
+	tests_without_specifier();
+	tests_for_strings();
+	tests_for_chars();
+	printf("\n====== Tests for %%i and %%d ======\n\n");
+	tests_for_ints("%d\n");
+	tests_for_ints("%i\n");
 	return (0);
 }
